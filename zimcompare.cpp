871155a29eb3c671f7cb6a4e7d0d579dc5b5bf5e
Kiran <kiran@Skynet.(none)>
2013-06-17 16:12:41 +0530

    Completed zimcompare tool. Ready for use, Fast enough.

--- zimcompare.cpp
+++ zimcompare.cpp
@@ -30,20 +30,17 @@
 #include <zim/fileiterator.h>
 #include <iostream>
 #include <string>
-#include <vector>
 #include <list>
-#include <algorithm>
-#include <regex>
 #include <ctime>
-#include <cstdio>
 
-class articleInfo
+class articleInfo                   //Class to store the title, namespace and hash of a file for comparison.
 {
     public:
     std::string Title;
     int Hash;
-    int index;
+    char Namespace;
 
+    //Comparison operator. Required for std::sort()
     bool operator <(articleInfo a)
     {
         return Title<a.Title?true:false;
@@ -65,13 +62,18 @@ int adler32(std::string buf)         //Adler32 Hash Function. Used to hash the B
 
 int main(int argc, char **argv)
 {
+    //Time variables in order to obtain time difference.
+    time_t startTime,endTime;
+    double  timeDiffference;
+    time(&startTime);
+
 
     //Parsing arguments
     //There will be only two arguments, so no detailed parsing is required.
-    //The first argument is the
+    std::cout<<"zimcompare";
     if(argc<3)
     {
-        std::cout<<"[ERROR] Not enough Arguments provided\n";
+        std::cout<<"\n[ERROR] Not enough Arguments provided\n";
         return -1;
     }
     std::string filename_1 =argv[1];
@@ -87,16 +89,22 @@ int main(int argc, char **argv)
         //Data structure for storing article-related information
         std::list<articleInfo> data_1;
         std::list<articleInfo> data_2;
-        data_1.clear();
-        data_2.clear();
+
+        //List of articles to be deleted
+        std::list<articleInfo> delete_list;
+        std::list<articleInfo> update_list;
+
 
         //Scanning Data from files, generating hash
         zim::Blob tempBlob;      //Temporary data storage for storing the BLOB of an article.
         std::string tempString; //Temporary data storage, in oder to convert the blob
         articleInfo tempArticle; //Temporary data storage, in order to push to data_1 and data_2 lists.
-        int counter=0;
         int length=0;
+
+        //Parsing through files, generating hashes.
+        std::cout<<"\n[INFO] Parsing through input ZIM files.."<<std::flush;
         //Parsing through file_1
+        std::cout<<"\n[INFO] Parsing through "<<filename_1<<std::flush;
         for (zim::File::const_iterator it = file_1.begin(); it != file_1.end(); ++it)
         {
             tempBlob=it->getData();
@@ -106,14 +114,12 @@ int main(int argc, char **argv)
                 tempString+=tempBlob.data()[i];
             tempArticle.Hash= adler32(tempString);
             tempArticle.Title=it->getTitle();
-            tempArticle.index=counter;
+            tempArticle.Namespace=it->getNamespace();
             data_1.push_back(tempArticle);
-            counter++;
         }
 
-
         //Parsing through file_2
-        counter=0;
+        std::cout<<"\n[INFO] Parsing through "<<filename_2<<std::flush;
         for (zim::File::const_iterator it = file_2.begin(); it != file_2.end(); ++it)
         {
             tempBlob=it->getData();
@@ -123,9 +129,8 @@ int main(int argc, char **argv)
                 tempString+=tempBlob.data()[i];
             tempArticle.Hash= adler32(tempString);
             tempArticle.Title=it->getTitle();
-            tempArticle.index=counter;
+            tempArticle.Namespace=it->getNamespace();
             data_2.push_back(tempArticle);
-            counter++;
         }
 
 
@@ -135,12 +140,55 @@ int main(int argc, char **argv)
 
 
         //Comparing articles using the data obtained through parsing.
-        std::list<articleInfo>::iterator prev=data_1.begin();
-        for(std::list<articleInfo>::iterator it=++prev;it!=data_1.end();++it)
+        std::cout<<"\n[INFO] Comparing articles.."<<std::flush;
+        int counter=0;
+        while(data_1.size()>0)
         {
-
+            std::list<articleInfo>::iterator it=data_1.begin();
+            //Search through data_2 for an article with the same Title.
+            for(std::list<articleInfo>::iterator sub_it=data_2.begin();sub_it!=data_2.end();++sub_it)
+            {
+                if(it->Title==sub_it->Title)
+                {
+                    //A match for an article with the same title.
+                    if(it->Hash==sub_it->Hash)
+                    {
+                        //The article content has been unchanged.
+                        counter++;
+                    }
+                    else
+                    {
+                        //The article content has changed.
+                        update_list.push_back(*it);
+                    }
+                    //Once an article has been found in data_2, it is deleted from data_2 since there is no more use for the article and since it
+                    //speeds up the remaining computation.
+                    data_2.erase(sub_it);
+                    break;
+                }
+                else
+                {
+                    //No Match for the article
+                    //Article has been deleted.
+                    delete_list.push_back(*it);
+                }
+            }
+            data_1.erase(it);
         }
 
+
+        //After the above search is complete, the elements listed in data_2 are the elements that need to be added
+
+        //Displaying output:
+        std::cout<<"\n[INFO] "<<delete_list.size()<<" articles deleted in "<<filename_2<<std::flush;
+        std::cout<<"\n[INFO] "<<update_list.size()<<" articles updated in "<<filename_2<<std::flush;
+        std::cout<<"\n[INFO] "<<update_list.size()<<" articles updated in "<<filename_2<<std::flush;
+        std::cout<<"\n[INFO] "<<counter<<" articles remained unchanged\n"<<std::flush;
+
+        //Computing time difference.
+        time(&endTime);
+        timeDiffference=difftime(endTime,startTime);
+        std::cout<<"\n[INFO]Total time taken by zimcompare: "<<timeDiffference<<" seconds.\n"<<std::flush;
 
     }
     catch (const std::exception& e)
